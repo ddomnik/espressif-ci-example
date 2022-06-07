4c1a9b2765ae1bda86307c68076b2393cc0d979d
U-AED\ddufner <ddufner@PCAED1701.aed.dir>
2022-06-07 08:49:50 +0200

    removed unit test build folder

--- main/module_geometry/unit_test/build/CMakeFiles/geometry_test.dir/cygdrive/c/gitrepos/espressif_ci_example/main/module_geometry/geometry.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/geometry_test.dir/cygdrive/c/gitrepos/espressif_ci_example/main/module_geometry/geometry.cpp.o: \
- /cygdrive/c/gitrepos/espressif_ci_example/main/module_geometry/geometry.cpp \
- /cygdrive/c/gitrepos/espressif_ci_example/main/module_geometry/geometry.h
